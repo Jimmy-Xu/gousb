2a4a7e9c9ad9eb0a26d9c96d28bda68fedb740f3
Jimmy Xu <xjimmyshcn@gmail.com>
2020-07-26 10:49:28 +0800

    add -v

--- lsusb/main.go
+++ lsusb/main.go
@@ -26,7 +26,8 @@ import (
 )
 
 var (
-	debug = flag.Int("debug", 0, "libusb debug level (0..3)")
+	verbose = flag.Bool("v", false, "verbose")
+	debug   = flag.Int("debug", 0, "libusb debug level (0..3)")
 )
 
 func main() {
@@ -43,28 +44,30 @@ func main() {
 	devs, err := ctx.OpenDevices(func(desc *gousb.DeviceDesc) bool {
 		// The usbid package can be used to print out human readable information.
 		fmt.Printf("%03d.%03d %s:%s %s\n", desc.Bus, desc.Address, desc.Vendor, desc.Product, usbid.Describe(desc))
-		fmt.Printf("  Protocol: %s\n", usbid.Classify(desc))
 
-		// The configurations can be examined from the DeviceDesc, though they can only
-		// be set once the device is opened.  All configuration references must be closed,
-		// to free up the memory in libusb.
-		for _, cfg := range desc.Configs {
-			// This loop just uses more of the built-in and usbid pretty printing to list
-			// the USB devices.
-			fmt.Printf("  %s:\n", cfg)
-			for _, intf := range cfg.Interfaces {
-				fmt.Printf("    --------------\n")
-				for _, ifSetting := range intf.AltSettings {
-					fmt.Printf("    %s\n", ifSetting)
-					fmt.Printf("      %s\n", usbid.Classify(ifSetting))
-					for _, end := range ifSetting.Endpoints {
-						fmt.Printf("      %s\n", end)
+		if *verbose {
+			fmt.Printf("  Protocol: %s\n", usbid.Classify(desc))
+
+			// The configurations can be examined from the DeviceDesc, though they can only
+			// be set once the device is opened.  All configuration references must be closed,
+			// to free up the memory in libusb.
+			for _, cfg := range desc.Configs {
+				// This loop just uses more of the built-in and usbid pretty printing to list
+				// the USB devices.
+				fmt.Printf("  %s:\n", cfg)
+				for _, intf := range cfg.Interfaces {
+					fmt.Printf("    --------------\n")
+					for _, ifSetting := range intf.AltSettings {
+						fmt.Printf("    %s\n", ifSetting)
+						fmt.Printf("      %s\n", usbid.Classify(ifSetting))
+						for _, end := range ifSetting.Endpoints {
+							fmt.Printf("      %s\n", end)
+						}
 					}
 				}
+				fmt.Printf("    --------------\n")
 			}
-			fmt.Printf("    --------------\n")
 		}
-
 		// After inspecting the descriptor, return true or false depending on whether
 		// the device is "interesting" or not.  Any descriptor for which true is returned
 		// opens a Device which is retuned in a slice (and must be subsequently closed).
